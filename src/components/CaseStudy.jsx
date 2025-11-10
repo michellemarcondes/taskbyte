47cdfe5292e714ec0c9791eacb3b7c906b07dfaf
michelle <mpinheiromarcondes@gmail.com>
2025-11-10 19:38:03 -0300

    Atualização do caso de sucesso

--- src/components/CaseStudy.jsx
+++ src/components/CaseStudy.jsx
@@ -6,24 +6,24 @@ export default function CaseStudy(){
       <div className="container two-col">
         <div>
           <div className="framed">
-            <img src={`${import.meta.env.BASE_URL}carsburguer.png`} alt="Case: Carl's Jr." style={{width:'100%',maxWidth:600}}/>
+            <img src={`${import.meta.env.BASE_URL}caseindisa.jpg`} alt="Case: Indisa." style={{width:'100%',maxWidth:600}}/>
           </div>
         </div>
         <div className="text-max">
-          <div className="h5">CONHEÇA NOSSOS CASES DE SUCESSO!</div>
+          <div className="h5">CONHEÇA NOSSO CASE DE SUCESSO!</div>
           <div className="mt-16"></div>
           <h2>PROBLEMA DE GESTÃO DE ESTOQUE</h2>
           <div className="mt-16"></div>
           <p>
-            Desafio do Carls Jr. Burgers: +20h/semana lançando planilhas • 15% de divergência no inventário • Perdas por vencimento.
+            Dores do Cliente: Inconsistencia dos dados • Perda de produtividade buscando dados • Atrasos por falta de comunicação clara entre os setores.
           </p>
           <div className="mt-16"></div>
           <p>
-            Solução da TaskByte foi criar um sistema unificado com rastreamento de lotes + dashboard em tempo real.
+            Solução da TaskByte foi criar um sistema gerenciamento de garantias, que controla o lançamento, análise e alguns indicadores referentes aos itens.
           </p>
           <div className="mt-16"></div>
           <p>
-            Resultado: 68% menos perdas • R$ 12 mil/ano economizados.
+            Resultado: Melhor comunicação • 58% mais produtividade • Menos perda de dados • Melhora na apresentação dos dados.
           </p>
         </div>
       </div>
